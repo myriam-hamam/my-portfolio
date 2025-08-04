3bee382f189b85a55d191e2d5aed355584d6dddf
myriam-hamam <155687205+myriam-hamam@users.noreply.github.com>
2025-08-05 00:20:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // The form submission logic was removed to use Formspree.
+    // You can add any other JavaScript functionality here.
+
+});
